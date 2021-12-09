6e9bca612d62416144af0c7d0c087162a18ad164
Joshua <joxuecont@gmail.com>
2021-12-08 22:23:56 -0400

    seccion dibujar mejorado + card con imagen

--- assets/scripts/canvas.js
+++ assets/scripts/canvas.js
@@ -1,39 +1,54 @@
-window.addEventListener('load', () => {
-    const canvas = document.querySelector('#canvas');
-    const ctx = canvas.getContext('2d');
+const canvas = document.querySelector('#drawing-board');
+const toolbar = document.querySelector('#toolbar');
+const ctx = canvas.getContext('2d');
 
-    // Set canvas size
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
 
-    // Variables
-    let painting = false;
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
 
-    const startPosition = () => {
-        painting = true; 
-        draw(e);
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
-
-    const finishedPosition = () => {
-        painting = false;
-        ctx.beginPath();
-    }
-
-    const draw = (e) => {
-        if(!painting) return;
-        ctx.lineWidth = 10;
-        ctx.lineCap = 'round';
-
-        ctx.lineTo(e.clientX, e.clientY);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY);
-    }
-
-    // Event Listeners
-    canvas.addEventListener('mousedown', startPosition);
-    canvas.addEventListener('mouseup', finishedPosition);
-    canvas.addEventListener('mousemove', draw);
-    canvas.addEventListener('touchstart', startPosition);
-    canvas.addEventListener('mouseout', finishedPosition);
 });
+
+toolbar.addEventListener('change', e => {
+    if (e.target.id === 'line-width') {
+        lineWidth = e.target.value;
+    }
+
+    if (e.target.id === 'stroke') {
+        ctx.strokeStyle = e.target.value;
+    }
+});
+
+const draw = (e) => {
+    if (!isPainting) return;
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY - canvasOffsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', e => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
