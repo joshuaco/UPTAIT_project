3945abeaeca06dab285903ee674fab8c41d1d67f
Joshua <joxuecont@gmail.com>
2021-12-12 23:21:30 -0400

    Rediseño index (3 cols) y membrete logo

--- assets/scripts/script_index.js
+++ assets/scripts/script_index.js
@@ -1,7 +1,7 @@
 const username = window.location.search.split('=')[1];
 
 if (username !== undefined) {
-    document.querySelector('.title').innerHTML = `Hola ${username}!`;
+    document.querySelector('.title').innerHTML = `Hola ${username}, bienvenido!`;
 } else {
     document.querySelector('.title').innerHTML = 'Hola!';
 }
